d8808d2827515486bad3a45e7061b3881673cb7d
Peter O'Connor <peter.ed.oconnor@gmail.com>
2019-02-06 17:13:15 +0000

    added initial files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "spiking-eqprop",
+    author = "peter",
+    version = 0,
+    install_requires = [''],
+    dependency_links = (),
+    scripts = [],
+    packages=find_packages(),
+    )
